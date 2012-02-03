6a70eacc2f7599af6ac97ec315cfbb363ca57828
David Cantrell <david@cantrell.org.uk>
2012-02-03 08:25:47 +0000

    saner rendering

--- lib/BGATournaments.pm
+++ lib/BGATournaments.pm
@@ -3,6 +3,10 @@ package BGATournaments;
 use strict;
 use warnings;
 
+use constant DEBUG => 1;
+use Data::Dumper;
+local $Data::Dumper::Indent = 1;
+
 use Dancer ':syntax';
 
 get '/tournaments'                => widget(Tournament => 'list');
@@ -10,48 +14,27 @@ get '/tournaments/:tournament_id' => widget(Tournament => 'details');
 
 get '/'                           => widget('Root');
 
-after sub {
-  my $response = shift;
-  use Data::Dumper;local $Data::Dumper::Indent=1;
-  $response->{request} = request();
-  my $route = request()->{_route_pattern};
-  my $template = template_mapper($route);
-  $response->{content} = template(
-    $template,
-    {
-      dancer => {
-        route => $route,
-        template => $template,
-      },
-      %{$response->{content}}
-    }
-  );
-
-  $response->{content} .= '<xmp>'.Dumper($response).'</xmp>';
-};
-
-sub template_mapper {
-  my $route = shift;
-  warn("Looking for $route\n");
-  my $map = {
-    '^/$'           => 'root',
-    '^/tournaments$' => 'list-tournaments',
-  };
-  foreach my $re (keys %{$map}) {
-    warn("Trying ".qr($re)."\n");
-    if($route =~ /$re/) { return $map->{$re}.'.tt'; }
-  }
-  return "errors/notemplate.tt";
-}
-
 sub widget {
-  my $widget = __PACKAGE__.'::'.shift;
+  my $widget = shift;
   my $method = shift() || 'run';
-  eval "use $widget; \@${widget}::ISA = '".__PACKAGE__."'; $widget->import()";
+  (my $template = lc("${widget}-$method.tt")) =~ s/::/-/g;
+  if(!-e "views/$template") {
+    $template = "errors-notemplate.tt";
+  }
+  $widget = __PACKAGE__.'::'.$widget;
+  eval "use $widget";
   if($@) {
     die("can't load $widget: $@\n");
   }
-  return sub { $widget->$method() };
+  return sub {
+    my $route = request()->{_route_pattern};
+    my $results = $widget->$method();
+    my $rendered = template $template, {
+      dancer => { route => $route, template => $template, },
+      %{$results}
+    };
+    $rendered .= '<xmp>'.Dumper($results).'</xmp>' if(DEBUG);
+  };
 }
 
 1;
